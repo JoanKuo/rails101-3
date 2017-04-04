3e020399c24147b181d2f9f01f9965b16201df85
JoanKuo <guo-lingjun@foxmail.com>
2017-04-04 23:33:57 +0800

    favorite & review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,6 +4,9 @@ before_action :authenticate_user! , :only => [:new, :create]
   def new
     @movie = Movie.find(params[:movie_id])
     @review = Review.new
+    if !current_user.is_member_of?(@movie)
+      redirect_to movie_path(movie),alert:"没收藏不行哦><"
+    end
   end
 
   def create
