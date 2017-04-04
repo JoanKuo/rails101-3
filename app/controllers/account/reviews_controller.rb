77a7cc9c480d195dcdc2ee5477a9fa5211c39243
JoanKuo <guo-lingjun@foxmail.com>
2017-04-04 22:27:49 +0800

    edit/destroy button done

--- app/controllers/account/reviews_controller.rb
+++ app/controllers/account/reviews_controller.rb
@@ -5,23 +5,4 @@ class Account::ReviewsController < ApplicationController
     @reviews = current_user.reviews
   end
 
-  def edit
-    @movie = Movie.find(params[:movie_id])
-    @review = Review.find(review_params)
-  end
-
-  def destroy
-    @review = Review.find(params[:movie_id])
-    @review.destroy
-    flash[:alert] = "评论已删除"
-    redirect_to account_path(reviews)
-  end
-
-    private
-
-    def review_params
-      params.require(:review).permit(:content)
-    end
-
-
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,6 +4,9 @@ before_action :authenticate_user! , :only => [:new, :create]
   def new
     @movie = Movie.find(params[:movie_id])
     @review = Review.new
+    if !current_user.is_member_of?(movie)
+      redirect_to movie_path(@movie), alert:"没收藏不行哦><"
+    end
   end
 
   def create
@@ -17,6 +20,33 @@ before_action :authenticate_user! , :only => [:new, :create]
     else
       render :new
     end
+  end
+
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.movie = @movie
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.movie = @movie
+    @review.user = current_user
+    if @review.save(review_params)
+      redirect_to account_reviews_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.movie = @movie
+    @review.destroy
+    flash[:alert] = "评论已删除"
+    redirect_to account_reviews_path
   end
 
   private
